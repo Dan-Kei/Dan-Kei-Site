b4e83bd01ca02b715c9951f2bf8a8d80ebf4ffe2
Dan-Kei <keinine99@gmail.com>
2022-03-13 14:28:20 +0900

    First

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title('ストリームリット超入門')
+
+st.write('DataFrame')
+
+df = pd.DataFrame({
+    '1列目':[1,2,3,5],
+    '2列目':[10,20,30,40]
+})
+
+st.write(df)
